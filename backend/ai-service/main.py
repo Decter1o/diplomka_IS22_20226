5619fa420750e76fdecd0afa2a0ad011bb21a5b8
Decter1o <prodevelper69@gmail.com>
2026-04-08 20:32:39 +0500

    1233245

--- backend/ai-service/main.py
+++ backend/ai-service/main.py
@@ -1,20 +1,25 @@
 import httpx
+import threading
 from fastapi import FastAPI
 
 app = FastAPI()
 
-app.on_event("startup")
+@app.on_event("startup")
 async def startup_event():
+    camera_dictionary = dict()
     try:
         async with httpx.AsyncClient() as client:
-            camera_dictionary = dict()
             response = await client.get("http://mediamtx:8080/v3/path/list")
+            response.raise_for_status()
             data = response.json()
             for item in data['items']:
-                name = item.get['name']
-                
-                camera_dictionary[name] = ""
+                name = item.get('name')
+                if name is not None:
+                    camera_dictionary[name] = f"rtsp://mediamtx:8554/{name}"
+                else:
+                    print("Camera name is missing in the response item.")
 
+            threads = [threading.Tread(target = , args=(name, url)) for name, url in camera_dictionary.items()]
 
 
     except:
